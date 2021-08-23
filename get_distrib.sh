7d19aabe40173a16afa667cafee5812392f0c049
florencepascual <86305580+florencepascual@users.noreply.github.com>
2021-08-23 15:29:21 +0200

    Update get_distrib.sh

--- get_distrib.sh
+++ get_distrib.sh
@@ -2,8 +2,5 @@
 
 git clone https://github.com/docker/docker-ce-packaging --depth 1
 
-DEB_LIST=`cd docker-ce-packaging/deb && ls -1d debian-* ubuntu-*`
-RPM_LIST=`cd docker-ce-packaging/rpm && ls -1d centos-* fedora-*`
-
-echo $DEB_LIST
-echo $RPM_LIST
+echo DEB_LIST=`cd docker-ce-packaging/deb && ls -1d debian-* ubuntu-*` >> env.list
+echo RPM_LIST=`cd docker-ce-packaging/rpm && ls -1d centos-* fedora-*` >> env.list
