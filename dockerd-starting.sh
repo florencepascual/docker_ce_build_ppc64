83600b2969d102f682e91171e8483feb69059300
florencepascual <florencepascual>
2021-08-26 19:58:16 +0200

    script prowjob.sh (work in progress)

---
+++ dockerd-starting.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Waiting for dockerd to start"
+TIMEOUT=10
+DAEMON="dockerd"
+i=0
+while [ $i -lt $TIMEOUT ] && ! /usr/bin/pgrep $DAEMON
+do
+    i=$((i+1))
+    sleep 2
+done
+pid=`/usr/bin/pgrep $DAEMON`
+if [ -z "$pid" ]
+then
+    echo "$DAEMON did not started after $(($TIMEOUT*2)) seconds"
+    exit 1
+else
+    echo "Found $DAEMON pid:$pid"
+    ps -aef | grep docker | grep -v grep
+    sleep 10
+    ps -aef
+    echo "Launching docker info"
+    docker info
+fi
