b425bca1cb02edbcae6188e8f160a9124652ef15
florencepascual <florencepascual>
2021-08-26 12:12:34 +0200

    removed debug lines

--- get_distrib.sh
+++ get_distrib.sh
@@ -28,7 +28,3 @@ fi
 # get the packages list in the env.list
 echo DEB_LIST=\"`cd docker-ce-packaging/deb && ls -1d debian-* ubuntu-*`\" >> env.list
 echo RPM_LIST=\"`cd docker-ce-packaging/rpm && ls -1d centos-* fedora-*`\" >> env.list
-
-# set the env variables
-DEB_LIST=\"`cd docker-ce-packaging/deb && ls -1d debian-* ubuntu-*`\"
-RPM_LIST=\"`cd docker-ce-packaging/rpm && ls -1d centos-* fedora-*`\"
