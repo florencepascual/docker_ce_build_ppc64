8f5cbb87f850a075f4f215ab5bc950b857e703b1
florencepascual <florencepascual>
2021-08-26 11:21:08 +0200

    get_distrib test

--- get_distrib.sh
+++ get_distrib.sh
@@ -16,8 +16,8 @@ fi
 #echo DEB_LIST=`cd docker-ce-packaging/deb && ls -1d debian-* ubuntu-*` >> env.list
 #echo RPM_LIST=`cd docker-ce-packaging/rpm && ls -1d centos-* fedora-*` >> env.list
 
-DEB_LIST=debian-bullseye debian-buster ubuntu-bionic ubuntu-focal ubuntu-groovy ubuntu-hirsute
-RPM_LIST=centos-7 centos-8 fedora-33 fedora-34
+DEB_LIST="debian-bullseye debian-buster ubuntu-bionic ubuntu-focal ubuntu-groovy ubuntu-hirsute"
+RPM_LIST="centos-7 centos-8 fedora-33 fedora-34"
 
 for PACKTYPE in RPM_LIST; do
   echo "There is $PACKTYPE"
