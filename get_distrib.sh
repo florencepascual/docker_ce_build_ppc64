8174eb3fbc361a7157c7d847f88db4f91171345a
florencepascual <florencepascual>
2021-08-26 11:00:01 +0200

    updated get_distrib to check if dir and file exist

--- get_distrib.sh
+++ get_distrib.sh
@@ -1,6 +1,15 @@
 #/bin/bash
 
-git clone https://github.com/docker/docker-ce-packaging --depth 1
+if [[ ! -d docker-ce-packaging ]]
+then
+    echo "no docker-ce-packaging"
+    git clone https://github.com/docker/docker-ce-packaging --depth 1
+fi
 
+if [[ ! -f env.list ]]
+then
+    echo "no env.list"
+    touch env.list
+fi
 echo DEB_LIST=`cd docker-ce-packaging/deb && ls -1d debian-* ubuntu-*` >> env.list
 echo RPM_LIST=`cd docker-ce-packaging/rpm && ls -1d centos-* fedora-*` >> env.list
