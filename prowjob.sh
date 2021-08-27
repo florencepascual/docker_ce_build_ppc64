6430fe19b2566ddba48d65014031e81d28b75b50
florencepascual <florencepascual>
2021-08-27 15:34:20 +0200

    update prowjob.sh test

--- prowjob.sh
+++ prowjob.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PATH_DOCKERD_ENTRYPOINT="/usr/local/bin"
+#PATH_DOCKERD_ENTRYPOINT="/usr/local/bin"
+PATH_DOCKERD_ENTRYPOINT=""
 PATH_DOCKERD_STARTING=""
 PATH_IMAGE_BUILD="quay.io/florencepascual"
 PATH_SCRIPT_BUILD=""
@@ -9,6 +10,8 @@ PATH_SCRIPT_TEST="/test"
 # check if new versions of docker-ce and containerd
 echo DOCKER_VERS=\"`git ls-remote --refs --tags https://github.com/moby/moby.git | cut --delimiter='/' --fields=3 | grep 'v20' | sort --version-sort | tail --lines=1`\" > env.list
 echo CONTAINERD_VERS=\"`git ls-remote --refs --tags https://github.com/containerd/containerd.git | cut --delimiter='/' --fields=3 | grep v1.4 | sort --version-sort | tail --lines=1`\" >> env.list
+
+cat env.list
 
 # docker daemon
 .${PATH_DOCKERD_ENTRYPOINT}/dockerd-entrypoint.sh &
