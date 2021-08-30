4b1d1a575a390877c7b2b54b45db7a27a2f78ab8
florencepascual <86305580+florencepascual@users.noreply.github.com>
2021-08-30 18:11:58 +0200

    Update get_distrib.sh
    changed name of file (2 env files, env.list with the docker-ce version and the containerd version and the hash commit for docker-ce-packaging and env_distrib.list for distros)

--- get_distrib.sh
+++ get_distrib.sh
@@ -9,22 +9,22 @@ else
     cd docker-ce-packaging && git pull --depth 1 && cd ..
 fi
 
-if [[ ! -f env.list ]]
+if [[ ! -f env_distrib.list ]]
 # if there is no env.list file, create the file
 then
-    touch env.list
+    touch env_distrib.list
 else
 # if there is already DEB_LIST or RPM_LIST, remove these lines
-    if grep -Fq "DEB_LIST" env.list
+    if grep -Fq "DEB_LIST" env_distrib.list
     then
-        sed -i '/^DEB_LIST/d' env.list
+        sed -i '/^DEB_LIST/d' env_distrib.list
     fi
-    if grep -Fq "RPM_LIST" env.list
+    if grep -Fq "RPM_LIST" env_distrib.list
     then 
-        sed -i '/^RPM_LIST/d' env.list
+        sed -i '/^RPM_LIST/d' env_distrib.list
     fi
 fi
 
-# get the packages list in the env.list
-echo DEB_LIST=\"`cd docker-ce-packaging/deb && ls -1d debian-* ubuntu-*`\" >> env.list
-echo RPM_LIST=\"`cd docker-ce-packaging/rpm && ls -1d centos-* fedora-*`\" >> env.list
+# get the packages list in the env_distrib.list
+echo DEB_LIST=\"`cd docker-ce-packaging/deb && ls -1d debian-* ubuntu-*`\" >> env_distrib.list
+echo RPM_LIST=\"`cd docker-ce-packaging/rpm && ls -1d centos-* fedora-*`\" >> env_distrib.list
