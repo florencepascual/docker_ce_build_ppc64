5790453b43e3aa91c949d82a92b9bf6867bb6772
florencepascual <florence.pascual@ibm.com>
2021-08-31 15:17:31 +0200

    updated check_env.sh

--- check_env.sh
+++ check_env.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-echo $0
+echo $1
 
-case $0 in
+case $1 in
 	env.list)
         # file which we edit with DOCKER_VERS, CONTAINERD_VERS and PACKAGING_REF
 		echo "env.list"
-        if ! grep -Fq "DOCKER_VERS" $0
+        if ! grep -Fq "DOCKER_VERS" $1
         # if there is no docker_ce version
         then
             echo "There is no version of docker_ce"
             exit 1
         fi
-        if ! grep -Fq "CONTAINERD_VERS" $0
+        if ! grep -Fq "CONTAINERD_VERS" $1
         # if there is no containerd version
         then 
             echo "There is no version of containerd"
             exit 1
         fi
-        if ! grep -Fq "PACKAGING_REF" $0
+        if ! grep -Fq "PACKAGING_REF" $1
         # if there is no reference of docker-ce-packaging (hash commit) 
         then
             echo "There is no reference of docker-ce-packaging"
@@ -27,13 +27,13 @@ case $0 in
 		;;
 	env_distrib.list)
 		echo "env_distrib.list"
-		if ! grep -Fq "DEB_LIST" $0
+		if ! grep -Fq "DEB_LIST" $1
         # if there is no deb_list
         then
             echo "There is no distro in DEB"
             exit 1
         fi
-        if ! grep -Fq "RPM_LIST" $0
+        if ! grep -Fq "RPM_LIST" $1
         # if there is no rpm_list
         then 
             echo "There is no distro in RPM"
