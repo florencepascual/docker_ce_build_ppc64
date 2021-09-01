280844c32e1d11484b4821d1c0f249edf34b7c68
florencepascual <florence.pascual@ibm.com>
2021-09-01 19:08:28 +0200

    updated push_COS and debug

--- push_COS.sh
+++ push_COS.sh
@@ -20,18 +20,29 @@ mkdir -p ${PATH_COS}/s3_$COS_BUCKET
 # mount the cos bucket
 s3fs ${COS_BUCKET} ${PATH_COS}/s3_${COS_BUCKET} -o url=${URL_COS} -o passwd_file=${PATH_PASSWORD} -o ibm_iam_auth
 
-# get the directory name
-docker-ce-$DOCKER_CE_VER-$DOCKER_CE_BUILD_TAG
-containerd-$CONTAINERD_VER-$CONTAINERD_BUILD_TAG
-
-
 # copy the builds into the COS Bucket ibm-docker-builds
-if [[ -d docker-ce-* ]]
+if [[ -d ${PATH_COS}/s3_$COS_BUCKET/docker-ce-* ]]
 then
-    cp docker-ce-* ${PATH_COS}/s3_$COS_BUCKET
+    # get the directory name "docker-ce-20.10-11" version without patch number then build tag
+    # DIR_DOCKER_VERS=$(eval "echo ${DOCKER_VERS} | sed -E 's|(v)([0-9.]+)([0-9]+)(.[0-9])|\2\3|'")
+    DIR_DOCKER_VERS=$(eval "echo $DOCKER_VERS | cut -d'v' -f2 | cut -d'.' -f1-2")
+    DOCKER_LAST_BUILD_TAG=$(ls -d ${PATH_COS}/s3_$COS_BUCKET/docker-ce-${DIR_DOCKER_VERS}-* | sort --version-sort | tail -1| cut -d'-' -f6)
+    DOCKER_BUILD_TAG=$((DOCKER_LAST_BUILD_TAG+1))
+    DIR_DOCKER=docker-ce-${DIR_DOCKER_VERS}-${DOCKER_BUILD_TAG}
+    # copy the package to the cos bucket
+    ech ${DIR_DOCKER}
+    # cp docker-ce-* ${PATH_COS}/s3_$COS_BUCKET/${DIR_DOCKER}
 fi
-if [[ -d containerd-* ]]
+if [[ -d ${PATH_COS}/s3_$COS_BUCKET/containerd-* ]]
 then
-    cp containerd-* ${PATH_COS}/s3_$COS_BUCKET
+    # get the directory name "containerd-1.4-9" version without patch number then build tag
+    # DIR_CONTAINERD_VERS=$(eval "echo ${CONTAINERD_VERS} | sed -E 's|(v)([0-9.]+)([0-9]+)(.[0-9])|\2\3|'")
+    DIR_CONTAINERD_VERS=$(eval "echo $CONTAINERD_VERS | cut -d'v' -f2 | cut -d'.' -f1-2")
+    CONTAINERD_LAST_BUILD_TAG=$(ls -d ${PATH_COS}/s3_$COS_BUCKET/containerd-${DIR_CONTAINERD_VERS}-* | sort --version-sort | tail -1| cut -d'-' -f5)
+    CONTAINERD_BUILD_TAG=$((CONTAINERD_LAST_BUILD_TAG+1))
+    DIR_CONTAINERD=containerd-${DIR_CONTAINERD_VERS}-${CONTAINERD_BUILD_TAG}
+    # copy the package to the cos bucket
+    echo ${DIR_CONTAINERD}
+    # cp containerd-* ${PATH_COS}/s3_$COS_BUCKET/${DIR_CONTAINERD}
 fi
 
