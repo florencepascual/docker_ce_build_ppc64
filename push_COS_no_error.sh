da0d8de4f7aed81ab6c0b03876cb6c9f96fefaa7
florencepascual <86305580+florencepascual@users.noreply.github.com>
2021-09-03 15:09:42 +0200

    Update push_COS_no_error.sh

--- push_COS_no_error.sh
+++ push_COS_no_error.sh
@@ -78,12 +78,12 @@ mkdir -p ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}
 s3fs ${COS_BUCKET_PRIVATE} ${PATH_COS}/s3_${COS_BUCKET_PRIVATE} -o url=${URL_COS_PRIVATE} -o passwd_file=${PATH_PASSWORD} -o ibm_iam_auth
 
 # remove last version of docker-ce
-# rm -rf ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/docker-ce/docker-ce-*
+# rm -rf ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/prow-docker/docker-ce-*
 
 # copy the builds into the COS Bucket ppc64le-docker
 DIR_DOCKER_PRIVATE=docker-ce-${DOCKER_VERS}
 # copy the package to the cos bucket
-# cp /workspace/docker-ce-* ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/docker-ce/${DIR_DOCKER_PRIVATE}
+# cp /workspace/docker-ce-* ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/prow-docker/${DIR_DOCKER_PRIVATE}
 echo "${DIR_DOCKER_PRIVATE} copied"
 
 
@@ -91,12 +91,12 @@ if [[ ${CONTAINERD_VERS} != "0" ]]
 # if CONTAINERD_VERS contains a version of containerd
 then
     # remove last version of containerd
-    # rm -rf ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/docker-ce/containerd-*
+    # rm -rf ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/prow-docker/containerd-*
 
     # copy the builds in the COS bucket ppc64le-docker
     DIR_CONTAINERD_PRIVATE=containerd-${CONTAINERD_VERS}
     # copy the package to the cos bucket
-    # cp /workspace/containerd-* ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/docker-ce/${DIR_CONTAINERD_PRIVATE}
+    # cp /workspace/containerd-* ${PATH_COS}/s3_${COS_BUCKET_PRIVATE}/prow-docker/${DIR_CONTAINERD_PRIVATE}
     echo "${DIR_CONTAINERD_PRIVATE} copied"
 fi
 
