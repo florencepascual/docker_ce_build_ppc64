862f86b574fcc0dec1ba5dba8364559b34598889
florencepascual <florence.pascual@ibm.com>
2021-09-07 19:02:16 +0200

    updated test_stop.sh

--- test_stop.sh
+++ test_stop.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-stop_docker_daemon() {
-  ps -e | grep $1 # dockerd $DAEMON
-  kill -9 $2 # pid $pid
-}
+bash ./dockerd-entrypoint.sh &
 
 source ./docker_ce_build_ppc64/dockerd-starting.sh
 echo $DAEMON
 echo $pid
-if ! test -d /root/.docker 
+
+if [ ! -z "$pid" ]
 then
-  mkdir /root/.docker
-  echo "$SECRET_AUTH" > /root/.docker/config.json
-fi
-if grep -Fq "index.docker.io" /root/.docker/config.json
-then
-    docker run hello-world
-    stop_docker_daemon $DAEMON $pid && exit 0
+    if ! test -d /root/.docker 
+    then
+    mkdir /root/.docker
+    echo "$SECRET_AUTH" > /root/.docker/config.json
+    fi
+    if grep -Fq "index.docker.io" /root/.docker/config.json
+    then
+        docker run hello-world
+        kill -9 $pid && exit 0
+    fi
 fi
