a07c11d38bfc78710f2b93a984c624e26cc8b284
florencepascual <florence.pascual@ibm.com>
2021-09-07 14:43:13 +0200

    Create test_stop.sh

---
+++ test_stop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+stop_docker_daemon() {
+  ps -e | grep $1 # dockerd $DAEMON
+  kill -9 $2 # pid $pid
+}
+
+. ./docker_ce_build_ppc64/dockerd-starting.sh
+echo $DAEMON
+echo $pid
+if ! test -d /root/.docker 
+then
+  mkdir /root/.docker
+  echo "$SECRET_AUTH" > /root/.docker/config.json
+fi
+if grep -Fq "index.docker.io" /root/.docker/config.json
+then
+    docker run hello-world
+    stop_docker_daemon $DAEMON $pid
+fi
