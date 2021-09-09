d484ca34c3de857c781370b0da62f49818f48a2f
florencepascual <florence.pascual@ibm.com>
2021-09-09 17:57:27 +0200

    changed GOPATH test_launch.sh

--- test_launch.sh
+++ test_launch.sh
@@ -6,7 +6,7 @@ set -ue
 . ./docker_ce_build_ppc64/dockerd-starting.sh
 
 echo "Starting the docker test suite for:$1"
-export GOPATH=${WORKSPACE}/test:/go
+export GOPATH=${WORKSPACE}/dockertest:/go
 export GO111MODULE=auto
 cd /workspace/dockertest
 make test WHAT="./tests/$1" GOFLAGS="-v"
