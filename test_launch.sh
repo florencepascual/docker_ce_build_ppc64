06bae4ad4ab89fc53c9cd1e2cc6b28d102fd6b09
florencepascual <florence.pascual@ibm.com>
2021-09-09 19:48:44 +0200

    test

--- test_launch.sh
+++ test_launch.sh
@@ -6,9 +6,9 @@ set -ue
 . ./docker_ce_build_ppc64/dockerd-starting.sh
 
 echo "Starting the docker test suite for:$1"
-export GOPATH=${WORKSPACE}/dockertest:/go
+export GOPATH=${WORKSPACE}/test:/go
 export GO111MODULE=auto
-cd /workspace/dockertest
+cd /workspace/test/src/github.ibm.com/powercloud/dockertest
 make test WHAT="./tests/$1" GOFLAGS="-v"
 
 echo "End of the docker test suite"
