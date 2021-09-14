1b30d7e73cd252c9a476c2217c798e84304fe546
florencepascual <86305580+florencepascual@users.noreply.github.com>
2021-09-14 13:13:44 +0200

    fix DISTRO_NAME env in test_launch.sh

--- hack/docker-in-docker/test_launch.sh
+++ hack/docker-in-docker/test_launch.sh
@@ -5,13 +5,15 @@ set -ue
 
 . ./docker_ce_build_ppc64/hack/docker-in-docker/dockerd-starting.sh
 
+echo ${DISTRO_NAME}
+
 if ! test -d /root/.docker
 then
     mkdir /root/.docker
     echo "${SECRET_AUTH}" > /root/.docker/config.json
 fi
 
-echo "Starting the docker test suite for:$1"
+echo "Starting the docker test suite for:${DISTRO_NAME}"
 export GOPATH=${WORKSPACE}/test:/go
 export GO111MODULE=auto
 cd /workspace/test/src/github.ibm.com/powercloud/dockertest
