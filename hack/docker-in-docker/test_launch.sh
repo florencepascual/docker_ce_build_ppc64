490154b3305e5b966bf35e25bc8045fc5334b808
florencepascual <86305580+florencepascual@users.noreply.github.com>
2021-09-14 13:14:03 +0200

    fix DISTRO_NAME env in test_launch.sh

--- hack/docker-in-docker/test_launch.sh
+++ hack/docker-in-docker/test_launch.sh
@@ -17,7 +17,7 @@ echo "Starting the docker test suite for:${DISTRO_NAME}"
 export GOPATH=${WORKSPACE}/test:/go
 export GO111MODULE=auto
 cd /workspace/test/src/github.ibm.com/powercloud/dockertest
-make test WHAT="./tests/$1" GOFLAGS="-v"
+make test WHAT="./tests/${DISTRO_NAME}" GOFLAGS="-v"
 
 echo "End of the docker test suite"
 
