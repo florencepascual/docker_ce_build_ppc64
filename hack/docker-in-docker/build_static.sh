ae094f8586fb1ba018076024d5aaf653c787625b
florencepascual <florence.pascual@ibm.com>
2021-10-06 15:46:52 +0200

    build_static

---
+++ hack/docker-in-docker/build_static.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Script building the static docker packages
+
+set -ue
+
+set -o allexport
+source env.list
+source env-distrib.list
+
+DIR_DOCKER="/workspace/docker-ce-${DOCKER_VERS}"
+if ! test -d ${DIR_DOCKER}
+then
+  mkdir ${DIR_DOCKER}
+fi
+
+#Workaround for builkit cache issue where fedora-32/Dockerfile
+# (or the 1st Dockerfile used by buildkit) is used for all fedora's version
+# See https://github.com/moby/buildkit/issues/1368
+patchDockerFiles() {
+  Dockfiles="$(find $1  -name 'Dockerfile')"
+  d=$(date +%s)
+  i=0
+  for file in $Dockfiles; do
+      i=$(( i + 1 ))
+      echo "patching timestamp for $file"
+      touch -d @$(( d + i )) "$file"
+  done
+}
+
+echo "Populating docker-ce-packaging from git ref=$PACKAGING_REF"
+
+PACKAGING_DIR="docker-ce-packaging"
+mkdir $PACKAGING_DIR
+pushd $PACKAGING_DIR
+
+git init
+git remote add origin  https://github.com/docker/docker-ce-packaging.git
+git fetch --depth 1 origin $PACKAGING_REF
+git checkout FETCH_HEAD
+
+
+echo "populate docker-ce-packaging/src folders"
+make REF=${DOCKER_VERS} checkout
+popd
+
+echo "building static"
+pushd docker-ce-packaging/static
+#patchDockerFiles .
+VERSION=${DOCKER_VERS} CONTAINERD_VERSION=v1.4.11 RUNC_VERSION=v1.0.2 make static-linux
+popd
+
+cp docker-ce-packaging/static/build/linux/*.tgz ${DIR_DOCKER}
+
+pushd ${DIR_DOCKER}
+FILES="*"
+for f in $FILES
+do
+  mv $f "${f//${DOCKER_VERS}/ppc64le}"
+done
+popd
