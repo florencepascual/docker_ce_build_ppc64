e6e124a7f0fd3e59914216fc3eb8fe2a54759555
florencepascual <florence.pascual@ibm.com>
2021-10-08 18:04:54 +0200

    Update build_static.sh

--- build_static.sh
+++ build_static.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Script building the static docker packages
 # in the directory docker-ce-v20.10.9 where there is a static directory with the script
-# docker run -d -v /home/fpascual/docker-ce-v20.10.9:/workspace --privileged quay.io/powercloud/docker-ce-build -name docker-build-static 
+# docker run -d -v /home/fpascual/testing-prow-job:/workspace --privileged --name docker-build-static quay.io/powercloud/docker-ce-build
 # docker exec -it docker-build-static /bin/bash
 # ./static/build_static.sh
+# docker run -d -v /home/fpascual/testing-prow-job:/workspace --env PATH_SCRIPTS --privileged --name docker-build-static quay.io/powercloud/docker-ce-build ./docker_ce_build_ppc64/build_static.sh
 
 set -ue
 
@@ -11,46 +12,15 @@ set -o allexport
 source env.list
 source env-distrib.list
 
-DIR_DOCKER="/workspace/docker-ce-${DOCKER_VERS}"
-if ! test -d ${DIR_DOCKER}
-then
-  mkdir ${DIR_DOCKER}
-fi
-
-#Workaround for builkit cache issue where fedora-32/Dockerfile
-# (or the 1st Dockerfile used by buildkit) is used for all fedora's version
-# See https://github.com/moby/buildkit/issues/1368
-patchDockerFiles() {
-  Dockfiles="$(find $1  -name 'Dockerfile')"
-  d=$(date +%s)
-  i=0
-  for file in $Dockfiles; do
-      i=$(( i + 1 ))
-      echo "patching timestamp for $file"
-      touch -d @$(( d + i )) "$file"
-  done
-}
-
-echo "Populating docker-ce-packaging from git ref=$PACKAGING_REF"
-
-PACKAGING_DIR="docker-ce-packaging"
-mkdir $PACKAGING_DIR
-pushd $PACKAGING_DIR
-
-git init
-git remote add origin  https://github.com/docker/docker-ce-packaging.git
-git fetch --depth 1 origin $PACKAGING_REF
-git checkout FETCH_HEAD
-
-
-echo "populate docker-ce-packaging/src folders"
-make REF=${DOCKER_VERS} checkout
-popd
-
 echo "building static"
 pushd docker-ce-packaging/static
-#patchDockerFiles .
-VERSION=${DOCKER_VERS} CONTAINERD_VERSION=v1.4.11 RUNC_VERSION=v1.0.2 make static-linux
+# get the latest version of runc
+RUNC_VERS=$(eval "git ls-remote --refs --tags https://github.com/opencontainers/runc.git | cut --delimiter='/' --fields=3 | sort --version-sort | tail --lines=1")
+echo "VERSION=${DOCKER_VERS} CONTAINERD_VERSION=${CONTAINERD_VERS} RUNC_VERSION=${RUNC_VERS} make static-linux" > ${PATH_SCRIPTS}/build_static.sh
+chmod a+x ${PATH_SCRIPTS}/build_static.sh
+
+docker run -d -v /home/fpascual/testing-prow-job/test-static:/workspace --privileged --env DOCKER_VERS --env CONTAINERD_VERS --env RUNC_VERS --name docker-build-static2 quay.io/powercloud/docker-ce-build /bin/bash -c '${PATH_SCRIPTS}/build_static.sh'
+
 popd
 
 cp docker-ce-packaging/static/build/linux/*.tgz ${DIR_DOCKER}
